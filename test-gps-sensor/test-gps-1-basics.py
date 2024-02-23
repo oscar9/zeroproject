a770605c383872482cd38058272a29837ac54cb6
Óscar Martínez <oscar9@gmail.com>
2024-02-23 20:13:28 +0100

    Update test-gps-1-basics.py

--- test-gps-sensor/test-gps-1-basics.py
+++ test-gps-sensor/test-gps-1-basics.py
@@ -1,16 +1,22 @@
+
+# https://aprs.gids.nl/nmea/
 import serial
 import time
 import string
 import pynmea2
-# https://aprs.gids.nl/nmea/
+
 while True: 
     port="/dev/ttyAMA0"
     ser=serial.Serial(port,baudrate=9600,timeout=0.5)
     dataout =pynmea2.NMEAStreamReader()
     newdata=ser.readline()
-    if newdata[0:6]=="$GPRMC":
-        newmsg=pynmea2.parse(newdata)
+    #print("Next: ", str(newdata[0:6]), " @all. ", str(newdata))
+    code = newdata[0:6]
+    gpscode = b"$GPRMC"
+    #print("Code: ", code, " compare with", gpscode, " -- result: ", code=="$GPRMC")
+    if code==gpscode:
+        newmsg=pynmea2.parse(newdata.decode("utf-8"))
         lat=newmsg.latitude
         lng=newmsg.longitude
-        gps="Latitude=" +str(lat) + "and Longitude=" +str(lng)
+        gps="---- Latitude=" +str(lat) + "and Longitude=" +str(lng)
         print(gps)
