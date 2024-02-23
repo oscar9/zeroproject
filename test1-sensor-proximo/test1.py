93d15722a0e593d28a15b068f9b8c0f454be7172
Óscar Martínez <oscar9@gmail.com>
2024-02-23 16:24:09 +0100

    Update test1.py

--- test1-sensor-proximo/test1.py
+++ test1-sensor-proximo/test1.py
@@ -6,7 +6,11 @@ PIR_PIN = 14
 GPOI.setmode(GPIO.BCM)
 GPOI.setup(PIR_PIN, GPIO.IN)
 
+print("TEST: Sensor 1")
+print("Init..")
 while True:
     if GPIO.input(PIR_PIN):
         print("Motion detected")
     time.sleep(0.5)
+
+print("..Exit")
